079760c4dd55d08120d1aed2fb02bfdecf1e1994
tunkhine126 <tunkhine126@gmail.com>
2019-11-12 16:40:44 -0500

    CORS part II

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
  allow do
-   origins ‘*’
-   
-   resource ‘*’,
+   origins '*'
+
+   resource '*',
      headers: :any,
      methods: [:get, :post, :put, :patch, :delete, :options, :head]
  end
